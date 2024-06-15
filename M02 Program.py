d3d5efbb07e1f2a9672654fcbaf57c76439cb21d
[Ethan <[ewheeler20@ivytech.edu]>
2024-06-15 19:57:10 -0400

    Added experimental dict

--- M02 Program.py
+++ M02 Program.py
@@ -1,2 +1,8 @@
 d = {}
-firstEntry = input("Enter the name of your student, or type 'ZZZ' to end:")
+while True:
+    key = input("Enter student's first and last name, or type ZZZ to end: ")
+    value = input("Enter that student's GPA: ")
+    d[key] = value
+    print (d)
+
+
