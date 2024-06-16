96b88a551458285c75daf3a08be20da3d808b178
Ethan Wheeler <ethan.wheeler1997@gmail.com>
2024-06-16 07:42:09 -0400

    Final logic, needs bugfixes

--- M02 Program.py
+++ M02 Program.py
@@ -1,17 +1,21 @@
+# Initializes lists
 deanList = []
 honorList = []
+
+# Main loop logic, appends name if value is within range, ends loop on 'ZZZ' input
 while True:
     name = input("Enter student's first and last name, or type ZZZ to end: ")
     if name == "ZZZ":
         break
     value = float(input("Enter that student's GPA: "))
     if value >= 2.5:
-        honorList += name
+        honorList.append(name)
     elif value >= 3.5:
-        deanList += name
-    
-print(honorList)
-print(deanList)
+        deanList.append(name)
+
+# Displays output          
+print("Honor's List: ",honorList)
+print("Dean's List: ",deanList)
     
 
 
