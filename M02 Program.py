c8f2724761cd2538b82562fe83ab7109c6c49621
[Ethan <[ewheeler20@ivytech.edu]>
2024-06-15 20:36:20 -0400

    Added exp. logic

--- M02 Program.py
+++ M02 Program.py
@@ -1,8 +1,13 @@
 d = {}
 while True:
     key = input("Enter student's first and last name, or type ZZZ to end: ")
-    value = input("Enter that student's GPA: ")
+    if key == "ZZZ":
+        break
+    value = float(input("Enter that student's GPA: "))
     d[key] = value
-    print (d)
+    if value >= 3.5:
+        d[key] + "made the Dean's List"
+    
+
 
 
